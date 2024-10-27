6ad3fd45ffc3c8ed1643c302504212c619f02818
Chandhruj02 <chandhruj02@gmail.com>
2024-10-27 21:54:39 +0530

    Second Commit

--- NumberBasedProblems/ReverseaNumber.java
+++ NumberBasedProblems/ReverseaNumber.java
@@ -2,19 +2,19 @@ package JavaProblems.NumberBasedProblems;
 
 import java.util.Scanner;
 
-public class SumOfDigits {
+public class ReverseaNumber {
 
 	public static void main(String[] args) {
-		System.out.println("Sum of Digits");
+		System.out.println("Reverse a Number");
 		Scanner in = new Scanner(System.in);
 		System.out.println("Enter a Number: ");
 		int a = in.nextInt();
-		int sum = 0;
+		String result = "" ;
 		while(a > 0) {
-		 sum = sum + a % 10;
-		 a = a / 10;
-		}
-		System.out.println("The Sum Of Digits: " + sum);
+			 result = result + a % 10 ;
+			 a = a / 10; 
+		} 
+			System.out.println("The Resvered Number " + result);
 		in.close();
 	}
 

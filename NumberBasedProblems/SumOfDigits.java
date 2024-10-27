823447ef8501e6a7c7dffcc3255806be23f5c1d5
Chandhruj02 <chandhruj02@gmail.com>
2024-10-27 21:31:08 +0530

    inital commit

---
+++ NumberBasedProblems/SumOfDigits.java
@@ -0,0 +1,20 @@
+package JavaProblems.NumberBasedProblems;
+
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		System.out.println("Sum of Digits");
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a Number: ");
+		int a = in.nextInt();
+		int sum = 0;
+		while(a > 0) {
+		 sum = sum + a % 10;
+		 a = a / 10;
+		}
+		System.out.println("The Sum Of Digits: " + sum);
+	}
+
+}

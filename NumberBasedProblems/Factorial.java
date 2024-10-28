fe9401450b2e78c870c45caf1df962c9d4feaa4d
Chandhruj02 <chandhruj02@gmail.com>
2024-10-28 12:18:10 +0530

    updated

---
+++ NumberBasedProblems/Factorial.java
@@ -0,0 +1,20 @@
+package JavaProblems.NumberBasedProblems;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		System.out.println("Factorial of the Number");
+		System.out.println("Enter a number: ");
+		Scanner in =new Scanner(System.in);
+		int a = in.nextInt();
+		int fact = 1;
+		for(int i=1;i<=a;i++) {
+			fact *= i;
+		}
+		System.out.println("Factorial of "+ a + " : "+fact);
+		in.close();
+	}
+
+}

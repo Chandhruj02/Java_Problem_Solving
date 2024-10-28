ce973a6159d55617ec65575bb90b330d3b9dc660
Chandhruj02 <chandhruj02@gmail.com>
2024-10-28 14:47:08 +0530

    10th upload

---
+++ NumberBasedProblems/PowerofNumber.java
@@ -0,0 +1,18 @@
+package JavaProblems.NumberBasedProblems;
+
+import java.util.Scanner;
+
+public class PowerofNumber {
+
+	public static void main(String[] args) {
+		System.out.println("Power of Numbers");
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int number = in.nextInt();
+		System.out.println("Enter power value: ");
+		int powervalue = in.nextInt();
+		int powerednum = (int)Math.pow(number,powervalue);  
+		System.out.println("Power of numbers "+number+"^"+powervalue+" is: "+ powerednum);
+	}
+
+}

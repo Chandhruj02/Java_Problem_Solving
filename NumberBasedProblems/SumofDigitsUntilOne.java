b4b271da0f94a5522934a1fc0566b7d9c88d5cf7
Chandhruj02 <chandhruj02@gmail.com>
2024-11-01 11:39:30 +0530

    Error Updated

--- NumberBasedProblems/SumofDigitsUntilOne.java
+++ NumberBasedProblems/SumofDigitsUntilOne.java
@@ -1,20 +1,32 @@
 package JavaProblems.NumberBasedProblems;
 
+import java.util.Scanner;
+
 //import java.util.Scanner;
 
 public class SumofDigitsUntilOne {
+	
+	static int sum(int num) {
+			int sum = 0;
+			while(num != 0) {
+				 sum = num % 10 + sum;
+				 num = num / 10; 
+				}
+		return sum;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Sum of Digits");
-	//	Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 		System.out.println("Enter a Number: ");
-		int a = 9875;
-		int sum = 0;
-		while(a > 0) {
-		 sum = sum + a % 10;
-		 a = a / 10;
+		int number = in.nextInt();
+		int DigitSum = 0;
+		while (number >= 10) {
+			DigitSum = sum(number);
+			number = DigitSum; 
 		}
-		System.out.println("The Sum Of Digits: " + sum);
-	//	in.close();
+		
+		System.out.println("The Sum Of Digits: " + DigitSum);
+		in.close();
 	}
 }

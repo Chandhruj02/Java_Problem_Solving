367555d31d34163e1e1d8642a2c8917feca8583b
Chandhruj02 <chandhruj02@gmail.com>
2024-11-01 11:47:12 +0530

    EvenOrOdd Program

---
+++ NumberBasedProblems/EvenOrOdd.java
@@ -0,0 +1,20 @@
+package JavaProblems.NumberBasedProblems;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+		System.out.println("Even or Odd");
+		System.out.println("Enter the number: ");
+		Scanner in = new Scanner(System.in);
+		int number = in.nextInt();
+		if(number % 2 == 0) {
+			System.out.println("Typed number is Even");
+		}
+		else 
+			System.out.println("typed number is Odd");
+		in.close();
+	}
+
+}

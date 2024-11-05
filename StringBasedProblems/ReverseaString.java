9e51fcd7b2295a352b606cdc544263f520da916d
Chandhruj02 <chandhruj02@gmail.com>
2024-11-05 19:17:03 +0530

    3rd Program Uploaded

--- StringBasedProblems/ReverseaString.java
+++ StringBasedProblems/ReverseaString.java
@@ -1,10 +1,20 @@
 package StringBasedProblems;
 
+import java.util.Scanner;
+
 public class ReverseaString {
 
 	public static void main(String[] args) {
 		System.out.println("Reverse a String");
-		
+		System.out.println("Enter a String: ");
+		Scanner in = new Scanner(System.in);
+		String str = in.next();
+		String revstr = "";
+		for(int i=0;i<str.length(); i++) {
+			revstr = str.charAt(i) + revstr;
+		}
+		System.out.println("Reversed String: "+ revstr);
+		in.close();
 	}
 	
 }

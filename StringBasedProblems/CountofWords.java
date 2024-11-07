dbb5c4f39a24293debb9aef812ec6f4ccb3240f9
Chandhruj02 <chandhruj02@gmail.com>
2024-11-07 20:50:02 +0530

    6th Program Uploaded

--- StringBasedProblems/CountofWords.java
+++ StringBasedProblems/CountofWords.java
@@ -3,19 +3,16 @@ package StringBasedProblems;
 import java.util.Scanner;
 
 public class CountofWords {
-	
-	static int wordcount(String str) {
-			int count = 0;
-			
-		return count;
-	}
-	
+		
 	public static void main(String[] args) {
 		System.out.println("Count of Words");
 		System.out.println("Enter a Sentance: ");
 		Scanner in = new Scanner(System.in);
 		String sentence = in.nextLine();
-
+		String[] words = sentence.trim().split("\\s+");
+		int wordCount = words.length;
+		System.out.println("Count of words: "+wordCount);
+		in.close();
 	}
 
 }
